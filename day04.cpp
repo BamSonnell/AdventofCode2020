1bdf994e5d2887e405def2985474ff42074e300b
BamSonnell <36522095+BamSonnell@users.noreply.github.com>
2020-12-09 00:57:59 -0800

    Update day04.cpp

--- day04.cpp
+++ day04.cpp
@@ -160,31 +160,3 @@ int validatePassport(passportStruct passport) {
 
 	return 0;
 }
-
-
-
-/*
-	byr (Birth Year) - four digits; at least 1920 and at most 2002.
-	iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-	eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-	hgt (Height) - a number followed by either cm or in:
-		If cm, the number must be at least 150 and at most 193.
-		If in, the number must be at least 59 and at most 76.
-	hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-	ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-	pid (Passport ID) - a nine-digit number, including leading zeroes.
-	cid (Country ID) - ignored, missing or not.
-
-	for (auto const& ptr : passports) {
-		if (ptr.byr.second == 1 && ptr.ecl.second == 1 && ptr.eyr.second == 1 && ptr.hcl.second == 1 && ptr.hgt.second == 1 && ptr.iyr.second == 1 && ptr.pid.second == 1) passSum++;
-	}
-
-	byr (Birth Year)
-	iyr (Issue Year)
-	eyr (Expiration Year)
-	hgt (Height)
-	hcl (Hair Color)
-	ecl (Eye Color)
-	pid (Passport ID)
-	cid (Country ID)
-*/
